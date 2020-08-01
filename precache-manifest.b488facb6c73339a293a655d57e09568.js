a0b3783181ca7675538078d2175ba1431a3a5388
anniephilip23 <anniephilip23@gmail.com>
2020-08-02 01:24:19 +0530

    Updates

---
+++ precache-manifest.b488facb6c73339a293a655d57e09568.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fbd188391f7d219734017bc8278854ad",
+    "url": "/ToDo/index.html"
+  },
+  {
+    "revision": "68417c6f19beebbaf76a",
+    "url": "/ToDo/static/css/main.97059847.chunk.css"
+  },
+  {
+    "revision": "97cd20e42e1a78bc1311",
+    "url": "/ToDo/static/js/2.249ea6be.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ToDo/static/js/2.249ea6be.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "68417c6f19beebbaf76a",
+    "url": "/ToDo/static/js/main.9d4cf59f.chunk.js"
+  },
+  {
+    "revision": "a64d1f8367d1550a5ad8",
+    "url": "/ToDo/static/js/runtime-main.e935b743.js"
+  }
+]);
